b06fc2a67e3c6dc51fbd9581033922034ecb1666
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-05-02 09:34:37 +0900

    Find from the entry that a user has

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -56,7 +56,7 @@ class EntriesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_entry
-    @entry = Entry.find(params[:id])
+    @entry = @user.entries.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
