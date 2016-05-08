5df98728931462577d68fb0483cab8900760d9c6
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-05-08 09:37:50 +0900

    Change the look for entries page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def gravatar_tag(user, options = {})
+    size = options.delete(:size) || 80
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    image_tag "//www.gravatar.com/avatar/#{gravatar_id}?default=mm", options
+    image_tag "//www.gravatar.com/avatar/#{gravatar_id}?default=mm&size=#{size}", options
   end
 
   def syntax_highlight(html)
@@ -30,5 +31,9 @@ module ApplicationHelper
 
   def editor_mode?
     controller_name == 'entries' && action_name.in?(['new', 'edit'])
+  end
+
+  def page_name
+    [controller_name, action_name].join(':')
   end
 end
