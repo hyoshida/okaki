ac79c2037a08f8bddccd649c65979cf4590e3874
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-05-08 22:36:19 +0900

    Install carrierwave gem

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,7 +1,9 @@
 class Advertisement < ActiveRecord::Base
+  mount_uploader :image, AdvertisementImageUploader
+
   validates :title, length: { maximum: 255 }
   validates :url, presence: true, length: { maximum: 255 }
-  validates :image, presence: true, length: { maximum: 255 }
+  validates :image, presence: true
 
   def title
     read_attribute(:title) || 'No Title'
---
+++ app/uploaders/advertisement_image_uploader.rb
@@ -0,0 +1,3 @@
+class AdvertisementImageUploader < CarrierWave::Uploader::Base
+  storage :file
+end
