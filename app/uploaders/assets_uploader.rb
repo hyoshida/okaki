a9231c2470d0d8e153bb062448403d60eabe6392
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-05-17 23:03:18 +0900

    Use uplaoded file`s MD5 as filename

--- app/uploaders/assets_uploader.rb
+++ app/uploaders/assets_uploader.rb
@@ -4,4 +4,14 @@ class AssetsUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore.pluralize}/#{model.id}"
   end
+
+  # Use file`s MD5 as filename
+  # from https://github.com/carrierwaveuploader/carrierwave/wiki/How-to:-Use-file%60s-MD5-as-filename
+  def md5
+    @md5 ||= Digest::MD5.hexdigest model.send(mounted_as).read.to_s
+  end
+
+  def filename
+    @name ||= "#{md5}#{File.extname(super).downcase}" if super
+  end
 end
