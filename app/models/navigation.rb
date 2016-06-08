ff0a4b25dc983c6f7e218a476639963ee1663710
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-06-08 21:28:26 +0900

    Cache the navigations

--- app/models/navigation.rb
+++ app/models/navigation.rb
@@ -17,4 +17,14 @@ class Navigation < ActiveRecord::Base
     nav
     footer
   )
+
+  class << self
+    def cache_key
+      location = all.where_values_hash['location'] if all.respond_to? :where_values_hash
+      page = all.current_page if all.respond_to? :current_page
+      maximum_updated_at = maximum(:updated_at)
+      timestamp = maximum_updated_at.utc.to_s(cache_timestamp_format) if maximum_updated_at
+      "#{model_name.cache_key}/all-#{[location, page, timestamp, count].compact.join('-')}"
+    end
+  end
 end
