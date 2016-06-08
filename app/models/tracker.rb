ad7cd34f0aff53c0f92423641353cbc817aaeeb3
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-06-08 19:30:43 +0900

    Add the location to Tracker

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,8 +1,17 @@
 class Tracker < ActiveRecord::Base
+  extend Enumerize
+
   validates :name, presence: true, length: { maximum: 255 }
   validates :tracker_id, length: { maximum: 255 }
   validates :code, presence: true
+  validates :location, presence: true
 
   scope :active, -> { where(active: true) }
   scope :by_newest, -> { reorder(created_at: :desc) }
+
+  enumerize :location, in: %i(
+    after_opening_body
+    before_closing_body
+    head
+  ), scope: true
 end
