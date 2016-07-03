6970fb54fdb8388fc2dfb77c1e84e55c5f05e9c6
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-07-04 01:26:13 +0900

    Redirect to profile page after signed in when user is not admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,14 +1,14 @@
 module Admin
   class ApplicationController < ::ApplicationController
     before_action :authenticate_user!
-    before_action :redirect_to_root_path, unless: -> { current_user.admin? }
+    before_action :redirect_to_profile_path, unless: -> { current_user.admin? }
 
     prepend_around_action :disable_friendly_id
 
     private
 
-    def redirect_to_root_path
-      redirect_to root_path
+    def redirect_to_profile_path
+      redirect_to profile_path
     end
 
     def disable_friendly_id(&action)
