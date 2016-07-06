6443a9f1debc1be003e5cc59e5b0eca61bd9f6cc
YOSHIDA Hiroki <hyoshida@appirits.com>
2016-07-06 18:53:32 +0900

    Add specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  describe '#nickname' do
+    context 'nickname is empty' do
+      it { expect(subject.nickname).to eq(subject.name) }
+    end
+
+    context 'nickname is present' do
+      before { subject.update(nickname: 'Julius Caesar') }
+      it { expect(subject.nickname).to eq('Julius Caesar') }
+    end
+  end
+
+  describe '#generate_name' do
+    it 'generates name from email' do
+      subject.email = "john@example.com"
+      subject.send(:generate_name)
+      expect(subject.name).to eq('john')
+    end
+  end
 end
